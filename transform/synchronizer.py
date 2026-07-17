6973775697fe053a56ea3970cc72b01d90c0bde3
Daniel Limaverde da Silva <daniellmvsilva@gmail.com>
2026-07-16 23:44:36 -0300

    feat: implement signal synchronization pipeline
    feat: implement signal synchronization pipeline

--- transform/synchronizer.py
+++ transform/synchronizer.py
@@ -1,19 +1,64 @@
+import numpy as np
+from pandas import DataFrame, Index
+
 from domain.mapped_session import MappedSession
 from domain.signal import Signal
+from domain.telemetry_feature import TelemetryFeature
 
 
 class Synchronizer:
-    def __init__(self, target_frequency: int = 100):
-        self.target_frequency = target_frequency
-
     def synchronize(self, session: MappedSession) -> MappedSession:
-        return session
+        reference_signal = self._reference_signal(session.signals)
+        reference_index = self._create_time_index(reference_signal)
 
-    def _reference_signal(self):
-        pass
+        synchronized_signals: dict[TelemetryFeature, Signal] = {}
 
-    def _synchronize_signal(self):
-        pass
+        for feature, signal in session.signals.items():
+            synchronized_signals[feature] = self._synchronize_signal(
+                reference_signal, reference_index, signal
+            )
 
-    def _foward_fill(self, signal: Signal) -> Signal:
-        return signal
+        return MappedSession(metadata=session.metadata, signals=synchronized_signals)
+
+    def _reference_signal(self, signals: dict[TelemetryFeature, Signal]) -> Signal:
+        reference_signal = signals.get(TelemetryFeature.SESSION_TIME)
+        if reference_signal is None:
+            raise ValueError("MappedSession must contain the SESSION_TIME signal!")
+        return reference_signal
+
+    def _synchronize_signal(
+        self, reference_signal: Signal, reference_index: Index, signal: Signal
+    ) -> Signal:
+        if reference_signal.frequency == signal.frequency:
+            return signal
+        signal_index = self._create_time_index(signal)
+        data = signal.data.copy()
+        data.index = signal_index
+        data = self._reindex_signal(data, reference_index)
+        data = self._foward_fill(data)
+        return self._create_synchronized_signal(
+            signal, reference_signal.frequency, data
+        )
+
+    def _create_time_index(self, signal: Signal) -> Index:
+        sample_period = 1 / signal.frequency
+        n_samples = len(signal.data.index)
+
+        time_index = Index(np.arange(n_samples) * sample_period, name="time")
+        return time_index
+
+    def _reindex_signal(self, data: DataFrame, reference_index: Index) -> DataFrame:
+        return data.reindex(reference_index)
+
+    def _foward_fill(self, data: DataFrame) -> DataFrame:
+        return data.ffill()
+
+    def _create_synchronized_signal(
+        self, original_signal: Signal, reference_frequency: int, data: DataFrame
+    ) -> Signal:
+        return Signal(
+            feature=original_signal.feature,
+            frequency=reference_frequency,
+            unit=original_signal.unit,
+            data=data,
+        )
